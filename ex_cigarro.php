bb4fe7d535f0525a3c090552c597d741416400cb
Izabella da Costa <130591081+Starlyyy@users.noreply.github.com>
2023-10-04 17:12:09 -0300

    Update ex_cigarro.php

--- ex_cigarro.php
+++ ex_cigarro.php
@@ -10,5 +10,7 @@ $dias_peridos = 0;
 $cigarros_fumados = $cigarros_dia * 365 * $tempo_fumado;
 $tempo_perdido_total = $cigarros_fumados * $tempo_perdido_por_cigarro;
 $dias_perdidos = $tempo_perdido_total / 60 / 24;
+$dias_perdidos = floor($dias_perdidos);
 
-print("Você perderá $dias_perdidos perdidos de vida\n");
+print("Você perderá $dias_perdidos dias de vida\n");
+
