02f2592246ba16bb15116e92425e7080cb20bf8f
Izabella da Costa <130591081+Starlyyy@users.noreply.github.com>
2023-10-25 17:50:09 -0300

    Create associativo.php

---
+++ associativo.php
@@ -0,0 +1,14 @@
+<?php
+
+$fruta = array(
+    
+    "cor" => "vermelha",
+    "sabor" => "doce",
+    "formato" => "redonda",
+    "nome" => "maçã"
+
+);
+
+foreach ($fruta as $caracteristica) {
+    print $caracteristica. "\n";
+}
