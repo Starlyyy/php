27247338496ca7d461cec79c5610bf103a8b9428
Izabella da Costa <130591081+Starlyyy@users.noreply.github.com>
2023-11-22 17:47:27 -0300

    Create pokemon.php

---
+++ pokemon.php
@@ -0,0 +1,26 @@
+<?php
+
+    $nome_pokemon = readline("Me dê o nome de um pokemon e eu lhe dou as informações sobre ele: ");
+
+    $dados_em_texto = file_get_contents("https://pokeapi.co/api/v2/pokemon/{$nome_pokemon}");
+
+    $pokemon = json_decode($dados_em_texto, true);
+
+    print("Nome: " . strtoupper($pokemon['name']) . "\n");
+    print("Movimentos: \n");
+
+    foreach ($pokemon['moves'] as $move) {
+        
+        print(" - " . $move['move']['name'] . "\n");
+
+    }
+
+    print("Altura: " . $pokemon['height'] . "\n");
+    print("Peso: " . $pokemon['weight'] . "\n");
+    print("Tipo(s):\n");
+
+    foreach ($pokemon['types'] as $type) {
+        
+        print(" - " . $type['type']['name'] . "\n");
+
+    }
