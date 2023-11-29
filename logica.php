6909d33a1c893d08913c982ae2b3c57715610b50
Izabella da Costa <130591081+Starlyyy@users.noreply.github.com>
2023-11-29 17:52:12 -0300

    Update logica.php

--- logica.php
+++ logica.php
@@ -1,3 +1,32 @@
+<?php
+
+    //include 'logica.php';
+
+    $pokemons_api = file_get_contents('https://pokeapi.co/api/v2/pokemon');
+    $pokemons = json_decode($pokemons_api, true);
+
+    for($i = 0; $i < 20; $i++){
+
+        $pokemon = $pokemons['results'][$i];
+
+        $todas_infos_api = file_get_contents($pokemon['url']);
+        $pokemons['results'][$i] = json_decode($todas_infos_api, true);
+
+    }
+
+
+    // print '<pre>';
+    // print_r($pokemons['results']);
+    // print '</pre>';
+    // die;
+
+    // $dados_em_texto = file_get_contents("https://pokeapi.co/api/v2/pokemon/{$nome_pokemon}");
+
+    // $pokemon = json_decode($dados_em_texto, true);
+
+
+?>
+
 <html>
     <head>
 
@@ -10,18 +39,19 @@
 
                 background: #c92626;
                 font-family: Verdana, Geneva, Tahoma, sans-serif;
-                padding: 20px; 
+                padding: 15px; 
                 text-align: center;
                 
             }
 
             .Pokémon {
                 
-                width: 20%;
-                border: solid 5px #F6BFC4;
-                padding: 15px; /* Margem interna */
+                width: 15%;
+                border: solid 4px #F6BFC4;
+                padding: 10px; /* Margem interna */
                 margin: 10px 10px 10px 10px;
                 float: left;
+                text-align: center;
 
             }
 
@@ -52,9 +82,9 @@
 
             <div class="Pokémon">
 
-                <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/700.png" alt="Sylveon" width="350px">
+                <img src="<?= $pokemons['results'][$i]['sprites']['other']['dream_world']['front_default']?>" alt="" width="350px">
                 
-                <h1>Sylveon</h1>
+                <h1><?= $pokemons['results'][$i]['name']?></h1>
                 <p>Peso: 23.5 kg</p>
                 <p>Altura: 1.0 m</p>
 
